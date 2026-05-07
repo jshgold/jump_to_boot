17c336c95186bf7b0a5c30717c0e4c80a5777090
devho <jshgold93@gmail.com>
2026-05-07 12:49:03 +0900

    H2 의존성 추가 기본 테스트 코드 추가

--- src/main/java/csh/back/controller/MainController.java
+++ src/main/java/csh/back/controller/MainController.java
@@ -2,12 +2,15 @@ package csh.back.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("index() 메서드 호출됨!");
+        return "sbb";
     }
 
     @GetMapping("/hello")
--- src/test/java/csh/back/SbbApplicationTests.java
+++ src/test/java/csh/back/SbbApplicationTests.java
@@ -3,11 +3,15 @@ package csh.back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class SbbApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
